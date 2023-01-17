52b97fdc4e4030805245ece90233a73fe78f0d0d
Muhammed Aksoy <99371051+aksoysoftware@users.noreply.github.com>
2023-01-17 04:13:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 import javax.swing.JFrame;
-import java.sql.SQLException;
+
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args)  {
 
 
 
